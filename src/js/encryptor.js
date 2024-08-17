f4b7fcc183ba75a4c1f7780a218a1679c41ca208
Jorge Manrique <georgemanc.28@gmail.com>
2024-08-17 19:59:06 +0000

    Se crea función para desencriptar texto

--- src/js/encryptor.js
+++ src/js/encryptor.js
@@ -29,8 +29,23 @@ btnEncriptar.addEventListener('click', function () {
     }
 });
 
+btnDesencriptar.addEventListener('click', function () {
+    if (texto.value === '') {
+        textoDefecto();
+    } else {
+        // Modificar estilos
+        textShowH2.classList.add('hidden');
+        textShowP.classList.remove('text-md');
+        textShowP.classList.add('text-lg', 'text-left');
+
+        // Se muestra el texto cifrado
+        textShowP.innerHTML = ''
+        textShowP.innerHTML = desencriptarTexto(texto.value);
+    }
+});
+
 /**
- * 
+ * Función para encriptar el texto
  * @param {string} texto 
  * @returns string
  */
@@ -60,6 +75,53 @@ function encriptarTexto(texto) {
     }
 
     return texto_encriptado;
+}
+
+/**
+ * Función para desencriptar el texto
+ * @param {string} texto 
+ * @returns string
+ */
+function desencriptarTexto(texto) {
+
+    texto = textoDesencriptado(texto, 'ai', 'a');
+    texto = textoDesencriptado(texto, 'enter', 'e');
+    texto = textoDesencriptado(texto, 'imes', 'i');
+    texto = textoDesencriptado(texto, 'ober', 'o');
+    texto = textoDesencriptado(texto, 'ufat', 'u');
+
+    return texto;
+}
+
+/**
+ * Busca que la palabra se encuentre en el texto y la remplaza
+ * @param {string} texto 
+ * @param {string} palabra 
+ * @param {string} cambiaPalabra 
+ * @returns string
+ */
+function validaSiPalabraExiste(texto, palabra, cambiaPalabra) {
+    if (texto.includes(palabra)) {
+        return texto.replaceAll(palabra, cambiaPalabra);
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Devuelve el nuevo texto
+ * @param {string} texto 
+ * @param {string} palabra 
+ * @param {string} remplazar 
+ * @returns string
+ */
+function textoDesencriptado(texto, palabra, remplazar) {
+    if (validaSiPalabraExiste(texto, palabra, remplazar)) {
+        texto = validaSiPalabraExiste(texto, palabra, remplazar);
+    } else {
+        texto = texto;
+    }
+    return texto;
 }
 
 function textoDefecto() {
